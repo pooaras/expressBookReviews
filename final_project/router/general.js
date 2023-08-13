9ca0bfd6cab0fa61b3e3997e4f6426701528f9dd
pooaras <pooarasusivaraj2002@gmail.com>
2023-08-13 13:26:04 -0400

    completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,57 +19,96 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-    res.send(JSON.stringify(books));
+    let promise= new Promise((resolve,reject)=>{
+        try{
+            resolve(JSON.stringify(books))
+        }
+        catch(err){
+            reject(err)
+        }
+    })
+    promise.then(
+        (msg)=>{res.send(msg)},
+        (err)=>{res.send(err)}
+    )
   
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-    isbn=req.params.isbn;
-    if(isbn)
-    res.send(books[isbn]);
-    else
-    return res.status(300).json({message: "not found"});
+    let promise=new Promise((resolve,reject)=>{
+        try{
+            isbn=req.params.isbn;
+            if(isbn)
+                resolve(books[isbn]);
+        }
+        catch(err){
+            reject("book not found")
+        }
+    })
+    promise.then(
+        (data)=>{res.send(data)},
+        (err)=>{res.send(err)}
+        )
+
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
     author=req.params.author;
     let matchedbooks=[]
-    if(author){
-        // let length=Object.keys(books).length;
-        for(const key in books){
-            if(books.hasOwnProperty(key) && books[key].author===author)
-                matchedbooks.push(books[key])
+    let promise=new Promise((resolve,reject)=>{
+        try{
+            if(author){
+                // let length=Object.keys(books).length;
+                for(const key in books){
+                    if(books.hasOwnProperty(key) && books[key].author===author)
+                        matchedbooks.push(books[key])
+                }
+            if(matchedbooks)
+                resolve(matchedbooks);
+            else
+                reject("books not found");
+            }
+
         }
-    if(matchedbooks)
-        res.send(matchedbooks);
-    else
-        return res.status(300).json({message: "not found"});    
-        
-    }
-    else
-        return res.status(300).json({message: "not found"});
+        catch(err){
+            reject("not found")
+        }
+    })
+    promise.then(
+        (data)=>{res.send(data)},
+        (err)=>{res.send(err)}
+    )
+    
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
     title=req.params.title;
     let matchedbooks=[]
-    if(title){
-        // let length=Object.keys(books).length;
-        for(const key in books){
-            if(books.hasOwnProperty(key) && books[key].title===title)
-                matchedbooks.push(books[key])
+    let promise=new Promise((resolve,reject)=>{
+        try{
+            if(title){
+                for(const key in books){
+                    if(books.hasOwnProperty(key) && books[key].title===title)
+                        matchedbooks.push(books[key])
+                }
+            if(matchedbooks)
+                resolve(matchedbooks);
+            else
+                reject("books not found");    
+                
+            }
         }
-    if(matchedbooks)
-        res.send(matchedbooks);
-    else
-        return res.status(300).json({message: "not found"});    
-        
-    }
-    else
-        return res.status(300).json({message: "not found"});
+        catch(err){
+            reject("not found")
+        }
+    })
+        promise.then(
+            (data)=>{res.send(data)},
+            (err)=>{res.send(err)}
+        )
 });
 
 //  Get book review
